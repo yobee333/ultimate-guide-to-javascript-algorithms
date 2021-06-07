abd15bbbb9d89e976f6cba8661fe66aa9814e9b3
Bridget Brevik <bridgetbrevik@outlook.com>
2021-06-07 11:49:34 -0500

    palindrome

--- Beginner/palindromeChecker/index-START.js
+++ Beginner/palindromeChecker/index-START.js
@@ -5,11 +5,36 @@ e.g palindromeChecker('racecar') // will return true
 
 
 
+//straightforward reverse the text and check with a boolean
 
 function palindromeChecker(text) {
-    v// Code goes here
+    let reversedText = text.toLowerCase().split('').reverse().join('')
+
+    return text === reversedText
+
 }
 
+//loop thru and compare characters lowercase and split into an array 2. .every() method to loop thru and perform check against the current letter and its index in the array as parameters. Then regurn the result of the comparison between letter and current letter occupying the position. The .every() would evaluate to true if the test passes in all cases and false if it does not. HOWEVER we have to stop the comparison halfway through the string which this does not do!
 
+function palindromeChecker(text) {
+    let charArray = text.toLowerCase().split('')
+
+    let result = charArray.every((letter, index) => {
+        return letter === charArray[charArray.length - index - 1]
+    })
+    return result
+}
+
+//OPTIMIZED
+
+function palindromeChecker(text){
+    let textLen = text.length
+    for (let i = 0; i < textLen/2; i++){
+        if(text[i] !== text[textLen - 1 - i]){
+            return false
+        }
+    }
+    return true
+}
 
 module.exports = palindromeChecker;
