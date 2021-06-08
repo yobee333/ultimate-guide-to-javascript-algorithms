c39cd21c2420eb918e1a71e46777223e3168697d
Bridget Brevik <bridgetbrevik@outlook.com>
2021-06-08 10:18:39 -0500

    loop vs filter

--- Beginner/falsyBouncer/index-START.js
+++ Beginner/falsyBouncer/index-START.js
@@ -5,10 +5,26 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
+// for...of loop to loop thru every value. if the value returns true, it gets pushed into the new array. If it's false, move on to the next one
+
 
 function falsyBouncer(array) {
-    // Code goes here
+    let result = []
+    for(value of array){
+        if(value){
+            result.push(value)
+        }
+    }
+    return result
 }
 
+
+// .filter() test each element with the Boolean function to see if true or false. Filter out false.
+
+function falsyBouncer(array){
+    return array.filter((value) => {
+        return Boolean(value)
+    })
+}
 
 module.exports = falsyBouncer
